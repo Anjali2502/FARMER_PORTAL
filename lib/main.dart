4b925c6550d954fe131ab1d4838be849eb1c53d6
Anjali2502 <69887046+Anjali2502@users.noreply.github.com>
2021-09-13 00:18:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:community_material_icon/community_material_icon.dart';
 
@@ -28,6 +29,22 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool isSignUp = true;
+  bool isRememberme = false;
+
+  late TapGestureRecognizer _tapGestureRecognizer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tapGestureRecognizer = TapGestureRecognizer()
+      ..onTap = (){
+        setState(() {
+          print('hello');
+      });
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -41,112 +58,121 @@ class _LoginPageState extends State<LoginPage> {
             colorBlendMode: BlendMode.darken,
           ),
 
-          Positioned(
-            top: 200,
-              child: Container(
+          //buildBottomHalfContainer(true),
 
-            height: 380,
-            padding: EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width-40,
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.white10,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  blurRadius: 15,
-                  spreadRadius: 5
-                )
-              ]
-            ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              isSignUp = false;
-                            });
-                          },
-                          child: Column(
-                            children: [
-                              Text("LOGIN",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: isSignUp? Colors.white10 : Colors.white
-                              ),
-                              ),
-                              if(!isSignUp)
-                              Container(
-                                margin: EdgeInsets.only(top: 3),
-                                height: 2,
-                                width: 55,
-                                color: Colors.orange,
-
-                              )
-                            ],
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              isSignUp = true;
-                            });
-                          },
-                          child: Column(
-                            children: [
-                              Text("SIGNUP",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: isSignUp ? Colors.white : Colors.white10
-                                ),
-                              ),
-                              if(isSignUp)
-                              Container(
-                                margin: EdgeInsets.only(top: 3),
-                                height: 2,
-                                width: 55,
-                                color: Colors.orange,
-
-                              )
-                            ],
-                          ),
-                        )
-
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: Column(
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 700),
+            curve: Curves.bounceInOut,
+            top: isSignUp ? 200 : 230,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 700),
+                curve: Curves.bounceInOut,
+                height: isSignUp ? 320 : 250,
+                padding: EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width-40,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 15,
+                    spreadRadius: 5
+                  )
+                ]
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          buildTextField(Icons.email_outlined,"Email-ID",false,true),
-                          buildTextField(Icons.lock_outline,"Password",true,false),
-                          buildTextField(Icons.lock_outline,"Confirm Password",true,false)
+                          GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                isSignUp = false;
+                              });
+                            },
+                            child: Column(
+                              children: [
+                                Text("LOGIN",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: isSignUp? Colors.white10 : Colors.white
+                                ),
+                                ),
+                                if(!isSignUp)
+                                Container(
+                                  margin: EdgeInsets.only(top: 3),
+                                  height: 2,
+                                  width: 55,
+                                  color: Colors.orange,
+
+                                )
+                              ],
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                isSignUp = true;
+                              });
+                            },
+                            child: Column(
+                              children: [
+                                Text("SIGNUP",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: isSignUp ? Colors.white : Colors.white10
+                                  ),
+                                ),
+                                if(isSignUp)
+                                Container(
+                                  margin: EdgeInsets.only(top: 3),
+                                  height: 2,
+                                  width: 55,
+                                  color: Colors.orange,
+
+                                )
+                              ],
+                            ),
+                          )
+
                         ],
                       ),
-                    ),
-                  ],
+                      if(isSignUp)
+                      buildSignupSection(),
+                      if(!isSignUp)
+                      buildSigninSection()
+                    ],
+                  ),
                 ),
           )
 
           ),
-          //buildBottomHalfContainer(true),
+
 
 
           buildBottomHalfContainer(false),
-          Positioned(
+          /*Positioned(
             top: MediaQuery.of(context).size.height-100,
               right: 0,
               left: 0,
               child: Column(
                 children: [
 
-                  Text("Or Sign Up with"),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                        "Or Sign Up with",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                   Row(
                     children: [
                       TextButton(
@@ -174,11 +200,89 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   )
                 ],
-              ))
+              ))*/
 
         ],
       ),
     );
+  }
+
+  Container buildSigninSection() {
+    return Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        buildTextField(Icons.mail_outline, "Email_id", false, true),
+                        buildTextField(Icons.lock_outline, "Password", true, false),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Checkbox(
+                                  value: isRememberme,
+                                  activeColor: Colors.white,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isRememberme = !isRememberme;
+                                    });
+                                  },
+                                ),
+                                Text(
+                                  'Remember me',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  print("hello");
+                                });
+                              },
+                              child: Text(
+                                'Forgot Password ?',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.red,
+                                ),
+                              ),
+
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+  }
+
+  Container buildSignupSection() {
+    return Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        buildTextField(Icons.email_outlined,"Email-ID",false,true),
+                        buildTextField(Icons.lock_outline,"Password",true,false),
+                        buildTextField(Icons.lock_outline,"Confirm Password",true,false),
+                        /*Container(
+                          width: 200,
+                          margin: EdgeInsets.only(top: 20),
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              recognizer: _tapGestureRecognizer,
+                              text: "Forgot Password?",
+                              style: TextStyle(color: Colors.red)
+                            ),
+                          ),
+                        )*/
+                      ],
+                    ),
+                  );
   }
   Widget buildTextField(IconData icon,String hintText,bool isPassword,bool isEmail){
     return Padding(
@@ -212,9 +316,11 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-  Positioned buildBottomHalfContainer(bool showShadow){
-    return Positioned(
-        top: 535,
+  Widget buildBottomHalfContainer(bool showShadow){
+    return AnimatedPositioned(
+        duration: Duration(milliseconds: 700),
+        curve: Curves.bounceInOut,
+        top: isSignUp ? 470 : 430,
         right: 0,
         left: 0,
         child: Center(
@@ -252,6 +358,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
               child: Container(
                 margin: EdgeInsets.only(top: 13),
+                padding: EdgeInsets.only(top: 5),
                 child: Column(
 
                   children: [
